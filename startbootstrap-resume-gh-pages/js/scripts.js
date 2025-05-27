2328ebe04fa16f37bea4b39f291ce402afb3dcf1
A113070039 <a113070039@mail.shu.edu.tw>
2025-05-27 09:46:03 +0800

    1

--- startbootstrap-resume-gh-pages/js/scripts.js
+++ startbootstrap-resume-gh-pages/js/scripts.js
@@ -32,3 +32,10 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+$('.panel-collapse').on('show.bs.collapse', function () {
+    $(this).siblings('.panel-heading').addClass('active');
+  });
+ 
+  $('.panel-collapse').on('hide.bs.collapse', function () {
+    $(this).siblings('.panel-heading').removeClass('active');
+  });
